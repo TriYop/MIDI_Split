c9b61a37a0bb8c62053f5f9e7247633f6f83d71f
TriYop <dev@yvanjanet.net>
2017-10-22 23:39:21 +0200

    Added test for writing valid midi files

--- tests/test_MIDISplit.py
+++ tests/test_MIDISplit.py
@@ -10,7 +10,7 @@ def test_write_empty_midi_file():
 def test_write_midi_file():
     output = MidiFile()
     output.add_track(name="test_track")
-
     output.ticks_per_beat=724
     output.type = 1
     MIDISplit.write_midi_file("TestOut.mid", output)
+
